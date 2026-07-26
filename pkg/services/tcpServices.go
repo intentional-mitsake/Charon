c6486ff2b07b69db493e9f33e964ab39f51c0f5f
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-26 09:56:50 +0545

    added upstream conn

--- pkg/services/tcpServices.go
+++ pkg/services/tcpServices.go
@@ -7,9 +7,21 @@ import (
 )
 
 var logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+var upstreamAddr = os.Getenv("UPSTREAM_ADDR")
 
 func HandleConnection(conn net.Conn) {
-	// auto close connection
 	defer conn.Close()
+	if upstreamAddr == "" {
+		upstreamAddr = "127.0.0.1:8848"
+	}
+	// 1. already got the tcp conn from client to the proxy(main.go)
+	// now open another conn to the upstream server(actual backend server that processes the requests)
+	// net.Dial creates a tcp connection as a client to the given address(upstreamAddr/backend server)
+	upstreamConn, err := net.Dial("tcp", upstreamAddr)
+	if err != nil {
+		logger.Error("Error connecting to upstream!", "error", err.Error())
+		return
+	}
+	defer upstreamConn.Close()
 	logger.Info("Accepted connection!", "IP", conn.RemoteAddr().String())
 }
