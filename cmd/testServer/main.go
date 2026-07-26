2003a16e04f258eb2d3037f9c302d566dd98c806
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-26 19:06:43 +0545

    debugging response

--- cmd/testServer/main.go
+++ cmd/testServer/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 )
+
+var logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
 
 func main() {
 	upstreamPort := os.Getenv("UPSTREAM_PORT")
@@ -13,11 +16,14 @@ func main() {
 	}
 	upstreamAddr := fmt.Sprintf(":%s", upstreamPort)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		response := "This is the response from PORT: " + upstreamPort
 		// HandleFunc takes a pattern(string) and a handler function
 		// the handler func takes a writer and a request, this writes the response to the writer
 		// when a requseet is made to port 8848 with pattern "/",
 		// it will do this, .i.e write this response to the ResponseWriter
-		fmt.Fprintf(w, "This is the response from PORT: %s", upstreamPort)
+		fmt.Fprint(w, response)
+		logger.Info("Responded to request", "Response", response)
 	})
+	logger.Info("Started listening", "PORT", upstreamPort)
 	http.ListenAndServe(upstreamAddr, nil)
 }
