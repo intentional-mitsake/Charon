43a6431c5f8815c48743750853aa9de1389bde8e
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-26 09:36:32 +0545

    tcp listener with configurable port

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,33 @@
-package cmd
+package main
+
+import (
+	"charon/pkg/services"
+	"log/slog"
+	"net"
+	"os"
+)
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8848"
+	}
+	addr := ":" + port
+	// 1. takes network and address(port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		logger.Error("Error creating listener!", "error", err.Error())
+		// exit if error as that means no listening
+		os.Exit(1)
+	}
+	logger.Info("Started listening", "PORT", port)
+	for {
+		// 2. accepts connections
+		conn, err := listener.Accept()
+		if err != nil {
+			logger.Error("Error accepting connection!", "error", err.Error())
+		}
+		go services.HandleConnection(conn)
+	}
+}
