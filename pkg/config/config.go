f573fb39bb3e4b13c4c19ae96bf86c08d6418548
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-07-27 16:59:44 +0545

    added acq and release

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "sync/atomic"
+
 /*
 GET /auth?params HTTP/1.1
 Host: api.example.com
@@ -25,9 +27,21 @@ func CreateHttpReq() HTTPRequest {
 // for least connections algo
 type Upstream struct {
 	Address     string // 127.0.0.1:8848
-	ActiveConns int    // 0
+	ActiveConns int64  // 0
 }
 
 func CreateUpstream(address string) Upstream {
 	return Upstream{Address: address, ActiveConns: 0}
+}
+
+func (u *Upstream) ACquire() {
+	// func is same as using:
+	// mu.Lock() -> counter++ -> mu.Unlock()
+	// diff is mu can cover blocks of code(mutli var), wheras this is only for a single var(int, pointer)
+	// also this is way faster & mu BLOCKS the thread, this doesnt
+	atomic.AddInt64(&u.ActiveConns, 1)
+}
+
+func (u *Upstream) Release() {
+	atomic.AddInt64(&u.ActiveConns, -1)
 }
